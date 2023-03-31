b2c504fc654cb6df13214b2e76efeada11435838
George Gorchev <g.gorchev@xbprime.com>
2023-03-31 11:34:23 +0000

    create config file

---
+++ src/lib/cfg.js
@@ -0,0 +1,10 @@
+  export const gameConfig = {
+    width: 400, // canvas width
+    height: 420, // canvas height
+    fps: 20, // frames per second (canvas update)
+    colors: {
+      bg: "#112222",
+      main: "#203e4a",
+      light: "#4e94b0",
+    },
+  };
