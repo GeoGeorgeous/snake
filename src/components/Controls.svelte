26ead541cac3d15d46da1a919d480815fb620341
George Gorchev <g.gorchev@xbprime.com>
2023-03-31 11:34:37 +0000

    create Controls component

---
+++ src/components/Controls.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    const handleKeydown = (event) => {
+        let code = event.keyCode;
+        let direction = "";
+
+        if (code === 82) dispatch("restart");
+
+        switch (code) {
+            case 87:
+            case 38:
+                direction = "up";
+                break;
+            case 68:
+            case 39:
+                direction = "right";
+                break;
+            case 83:
+            case 40:
+                direction = "down";
+                break;
+            case 65:
+            case 37:
+                direction = "left";
+                break;
+            default:
+                break;
+        }
+
+        if (direction) dispatch("directionChange", direction);
+    };
+</script>
+
+<svelte:window on:keydown|preventDefault={handleKeydown} />
